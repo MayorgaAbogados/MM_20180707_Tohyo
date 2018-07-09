9058672ee3fa05c35532637838c26ba5c4a01819
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-07-09 18:10:41 -0500

    asdfa
    asdfasdf

--- dist/mmSurveyApp/uploadForms.php
+++ dist/mmSurveyApp/uploadForms.php
@@ -4,9 +4,50 @@ $data = array("Volvo", "BMW", "Toyota");
 header('Content-Type: application/json');
 echo json_encode($data);
 */
-echo "Recibiendo archivos";
-print_r($_POST);
-print_r($_FILES['FileProposal']);
-print_r($_FILES['FileResource']);
-print_r($_FILES['FileConcept']);
+$t=time();
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$idNumber = $_POST['idNumber'];
+$idType = $_POST['idType'];
+$professionalCard = $_POST['professionalCard'];
+$cellphone = $_POST['cellphone'];
+$city = $_POST['city'];
+$address = $_POST['address'];
+$phone = $_POST['phone'];
+$undergraduateInstitution = $_POST['undergraduateInstitution'];
+$undergraduateYear = $_POST['undergraduateYear'];
+$graduateInstitution = $_POST['graduateInstitution'];
+$graduateYear = $_POST['graduateYear'];
+$area = $_POST['area'];
+$FileProposal = $_FILES['FileProposal'];
+$FileResource = $_FILES['FileResource'];
+$FileConcept = $_FILES['FileConcept'];
+
+$submitDate = date("Y-m-d",$t);
+
+$FileProposalname = $FileProposal['name'];
+$FileProposaltype = $FileProposal['type'];
+$FileProposalerror = $FileProposal['error'];
+$FileProposaltemp = $FileProposal['tmp_name'];
+
+
+
+
+//The path you wish to upload the image to
+$filesPath = "./files";
+
+if(is_uploaded_file($FileProposaltemp)) {
+
+    if(move_uploaded_file($FileProposaltemp, $filesPath . $FileProposalname)) {
+        echo "Sussecfully uploaded your image.";
+    }
+    else {
+        echo "Failed to move your image.";
+    }
+}
+else {
+    echo "Failed to upload your image.";
+}
+
 ?>
