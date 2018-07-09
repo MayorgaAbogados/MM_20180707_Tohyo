5179a6d7538f4d8a127b4e034cc4d16685192ea4
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-07-09 16:27:37 -0500

    asdf
    asdfa

---
+++ dist/mmSurveyApp/uploadForms.php
@@ -0,0 +1,10 @@
+<?php 
+/*
+$data = array("Volvo", "BMW", "Toyota");
+header('Content-Type: application/json');
+echo json_encode($data);
+*/
+print_r($_POST);
+print_r($_GET);
+print_r($_FILES);
+?>
