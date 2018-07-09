8423b9ff2c8a3d082255eb79cc00e7eede3e5877
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-07-09 17:10:19 -0500

    dfd
    dfg

--- dist/mmSurveyApp/uploadForms.php
+++ dist/mmSurveyApp/uploadForms.php
@@ -4,6 +4,7 @@ $data = array("Volvo", "BMW", "Toyota");
 header('Content-Type: application/json');
 echo json_encode($data);
 */
+echo "Recibiendo archivos";
 print_r($_POST);
 print_r($_FILES['FileProposal']);
 print_r($_FILES['FileResource']);
