918b9339170d32cc8ddc63aa327475fa55e8c5d9
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-07-10 11:58:03 -0500

    asdfasd
    asdfasdfasdf

--- src/app/backend/class/mmdb.php
+++ src/app/backend/class/mmdb.php
@@ -22,26 +22,4 @@ class MMDB{
 
 
 
-
-
-
-
-
-
-
-
-$Conn = new mysqli($DB_Host , $DB_Username, $DB_Password, $DB_Database);
-$ConnStatus = "";
-// Check connection
-if ($Conn->connect_error) {
-    die("Connection failed : " . $Conn->connect_error);
-    $ConnStatus = $ConnStatus."Database Connection : Error , ";
-}
-
-if (!$Conn->set_charset("utf8")) {
-    $ConnStatus = $ConnStatus."Database UTF8 Error :".$Conn->error);
-}
-
-
-
 ?>
