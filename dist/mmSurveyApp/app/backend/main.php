98f439dff2fa85f3b9424353301ade27bd0fd2d7
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-07-10 11:59:35 -0500

    <zxcv<zxcz

--- dist/mmSurveyApp/app/backend/main.php
+++ dist/mmSurveyApp/app/backend/main.php
@@ -6,13 +6,13 @@
 
   include './header.php';
   include './settings.php'
-  include './class/mmfile.php';
-  include './class/mmform.php';
+  //include './class/mmfile.php';
+  //include './class/mmform.php';
   include './class/mmdb.php';
 
-  $mFileProposal = new MMFile();
-  $mFileResource = new MMFile();
-  $mFileConcept = new MMFile();
+  //$mFileProposal = new MMFile();
+  //$mFileResource = new MMFile();
+  //$mFileConcept = new MMFile();
 
   $mmDB = new MMDB();
 
