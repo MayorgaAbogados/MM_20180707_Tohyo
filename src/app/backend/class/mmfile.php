b7687a94c2eb9d49f4646795c3ecc96327239fac
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-07-10 17:49:18 -0500

    sdf
    f

--- src/app/backend/class/mmfile.php
+++ src/app/backend/class/mmfile.php
@@ -21,16 +21,14 @@ class MMFile{
 
     $filesPath = "./files";
 
-    $this->folder = $_POST['idNumber']."_".$name;
     mkdir($filesPath);
-    mkdir($filesPath."/".$this->folder);
-
+    mkdir($filesPath."/".$_POST['idNumber']);
 
     if(is_uploaded_file($temp)) {
-      $newPath = $filesPath."/".$this->folder."/".$category."_". $name;
+      $newPath = $filesPath."/".$_POST['idNumber']."/".$category."_". $name;
       echo $newPath;
       if(move_uploaded_file($temp, $newPath)) {
-          echo "Sussecfully uploaded your image.";
+          echo "Sussfully uploaded your image.";
       }
       else {
           echo "Failed to move your image.";
