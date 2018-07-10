efad4f67ce717ac1fee20fd4145e149f40eab1c1
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-07-10 12:03:09 -0500

    asdfasdf
    sadf

--- dist/mmSurveyApp/app/backend/main.php
+++ dist/mmSurveyApp/app/backend/main.php
@@ -4,11 +4,11 @@
   // ----- Backend Script for MM Tohyo Forms ------//
   // ----------------------------------------------//
 
-  include './header.php';
-  include './settings.php'
-  //include './class/mmfile.php';
-  //include './class/mmform.php';
-  include './class/mmdb.php';
+  include('./header.php');
+  include('./settings.php');
+  include('./class/mmfile.php');
+  include('./class/mmform.php');
+  include('./class/mmdb.php');
 
   //$mFileProposal = new MMFile();
   //$mFileResource = new MMFile();
