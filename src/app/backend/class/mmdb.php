310af195347833eaae3ee994213466df107b93a6
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-07-10 12:37:22 -0500

    SS
    SS

--- src/app/backend/class/mmdb.php
+++ src/app/backend/class/mmdb.php
@@ -16,12 +16,16 @@ class MMDB{
     $this->uploadTime = date("Y-m-d",$t);
 
     // Database Connection //
-      $DB_Username = "mayorgac_abogado";
-  $DB_Password = "suaita.monteria";
-  $DB_Host     = "localhost";
-  $DB_Database   = "mayorgac_abogados";
-  $DB_Table = "db_especialistas";
-    $Conn = new mysqli($DB_Host , $DB_Username, $DB_Password, $DB_Database);
+    $DB_Username = "mayorgac_abogado";
+    $DB_Password = "suaita.monteria";
+    $DB_Host     = "127.0.0.1";
+    $DB_Database   = "mayorgac_abogados";
+    $DB_Table = "db_especialistas";
+    echo $DB_Database;
+    echo $DB_Host;
+    echo $DB_Password;
+    echo $DB_Username;
+    $Conn = new mysqli($DB_Host,$DB_Username,$DB_Password,$DB_Database);
     if ($Conn->connect_error) {
        $connStatus =  $connStatus.$Conn->connect_error;
     }
